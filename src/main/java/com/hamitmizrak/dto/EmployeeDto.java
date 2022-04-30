e89bd58b22ba46a9e2baa50e32a6207296496893
hamitmizrak <hamitmizrak@gmail.com>
2022-04-30 21:14:55 +0300

    EmployeeDto

---
+++ src/main/java/com/hamitmizrak/dto/EmployeeDto.java
@@ -0,0 +1,19 @@
+package com.hamitmizrak.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class EmployeeDto {
+
+    private Long employeeId;
+    private String employeeName;
+    private String employeeEmail;
+
+}
