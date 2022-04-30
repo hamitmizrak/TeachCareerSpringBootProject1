58adc3336eb3c5851e69bc62407f23c990498407
hamitmizrak <hamitmizrak@gmail.com>
2022-04-30 22:11:45 +0300

    EmployeeEntity

---
+++ src/main/java/com/hamitmizrak/data/entity/EmployeeEntity.java
@@ -0,0 +1,33 @@
+package com.hamitmizrak.data.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+
+
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity extends BaseEntiy implements Serializable {
+    public final static long serialVersionUID=1L;
+
+    @Column(name="employee_name")
+    private String employeeName;
+
+    @Column(name="employee_email")
+    private String employeeEmail;
+
+    //parametreli constructor
+    public EmployeeEntity( String employeeName, String employeeEmail) {
+        this.employeeName = employeeName;
+        this.employeeEmail = employeeEmail;
+    }
+}
