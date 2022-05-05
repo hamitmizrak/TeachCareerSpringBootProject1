5b0fd38f29e71be2b169b99f08913bef180a9e21
hamitmizrak <hamitmizrak@gmail.com>
2022-05-05 16:33:02 +0300

    Unit Test

--- src/test/java/com/hamitmizrak/TeachCareerSpringBootProject1ApplicationTests.java
+++ src/test/java/com/hamitmizrak/TeachCareerSpringBootProject1ApplicationTests.java
@@ -1,13 +1,76 @@
 package com.hamitmizrak;
 
+import com.hamitmizrak.data.entity.EmployeeEntity;
+import com.hamitmizrak.data.repository.EmployeeRepository;
+import com.hamitmizrak.iotest.ITestData;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TeachCareerSpringBootProject1ApplicationTests {
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TeachCareerSpringBootProject1ApplicationTests implements ITestData {
+
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    //SAVE
+    @Override
     @Test
-    void contextLoads() {
+    public void testCreate() {
+        EmployeeEntity entity=new EmployeeEntity();
+        entity.setEmployeeName("employeename44");
+        entity.setEmployeeEmail("hamitmizrak@gmail.com");
+        employeeRepository.save(entity);
+
+        //eger: 1 id data bulamazsa Exception fırlat: java.util.NoSuchElementException: No value present
+        assertNotNull(employeeRepository.findById(1L).get());
     }
 
+    //FIND
+    @Override
+    @Test
+    public void testFindbyId() {
+        EmployeeEntity entity=employeeRepository.findById(1L).get();
+        //Belirtilen id bulunmazsa: java.util.NoSuchElementException: No value present
+        assertEquals("employeename44",entity.getEmployeeName());
+    }
+
+    //LIST
+    @Override
+    @Test
+    public void testList() {
+     List<EmployeeEntity> entityList= employeeRepository.findAll();
+     //java.lang.AssertionError:  Expecting actual:0  to be greater than:
+     assertThat(entityList).size().isGreaterThan(0);
+    }
+
+    //UPDATE
+    @Override
+    @Test
+    public void testUpdate() {
+        EmployeeEntity entity=employeeRepository.findById(1L).get();
+        entity.setEmployeeName("employeename55");
+        employeeRepository.save(entity);
+
+        //org.opentest4j.AssertionFailedError: expected: not equal but was: <employeename55>
+        //unexpected:Beklenen
+        assertNotEquals("employeename44",employeeRepository.findById(1L).get().getEmployeeName());
+
+    }
+
+    //DELETE
+    @Override
+    @Test
+    public void testDelete() {
+        employeeRepository.deleteById(1L);
+        //istenilen data yoksa: No class com.hamitmizrak.data.entity.EmployeeEntity entity with id 1 exists!
+        assertThat(employeeRepository.existsById(1L)).isFalse();
+    }
 }
---
+++ src/test/java/com/hamitmizrak/iotest/ITestData.java
@@ -0,0 +1,19 @@
+package com.hamitmizrak.iotest;
+
+public interface ITestData {
+
+    //SAVE
+    public void testCreate();
+
+    //FIND
+    public void testFindbyId();
+
+    //LIST
+    public void testList();
+
+    //UPDATE
+    public void testUpdate();
+
+    //DELETE
+    public void testDelete();
+}
